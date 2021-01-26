b0cc9bd9a6e177657aad6ddb2cc6dba78e1c66a9
henryngann <henrynganwork@gmail.com>
2021-01-25 20:27:07 -0500

    finished all CRUD routes

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const app = express();
 //express middleware for req body
 app.use(express.json());
 //Get All Users
-app.get("/Users", async (req, res) => {
+app.get("/users", async (req, res) => {
   try {
     const response = await db.query("select * from users");
     console.log(response);
@@ -21,27 +21,81 @@ app.get("/Users", async (req, res) => {
   }
 });
 //Get One User
-app.get("/Users/:id", (req, res) => {
-  console.log(req.params.id);
+app.get("/users/:id", async (req, res) => {
+  try {
+    const response = await db.query(
+      `select * from users where id = ${req.params.id}`
+    );
+    res.json({
+      results: response.rows.length,
+      data: {
+        users: response.rows[0],
+      },
+    });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 //Create a User
 
-app.post("/Users/", (req, res) => {
+app.post("/users", async (req, res) => {
+  try {
+    const response = await db.query(
+      "INSERT INTO users (name,review) values ($1,$2) returning *",
+      [req.body.name, req.body.review]
+    );
+    console.log(response, req.body.name, req.body.review);
+
+    res.json({
+      results: response.rows.length,
+      data: {
+        users: response.rows[0],
+      },
+    });
+  } catch (error) {
+    console.log(error);
+  }
+
   console.log(req.body);
-});
-app.listen(4040, () => {
-  console.log("Listening");
 });
 
 //Update a user
 
-app.put("/Users/:id", (req, res) => {
-  console.log(req.params.id);
+app.put("/users/:id", async (req, res) => {
+  try {
+    const response = await db.query(
+      "UPDATE users SET name = $1, review = $2 where id = $3",
+      [req.body.name, req.body.review, req.params.id]
+    );
+
+    res.json({
+      results: response.rows.length,
+      data: {
+        users: response.rows[0],
+      },
+    });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 //delete a user
 
-app.delete("/Users/:id", (req, res) => {
+app.delete("/users/:id", async (req, res) => {
+  try {
+    const response = await db.query(
+      `DELETE FROM users where id = ${req.params.id}`
+    );
+    res.status(200).json({
+      status: "success",
+    });
+  } catch (err) {
+    console.log(err);
+  }
   console.log("deleted");
+});
+
+app.listen(4040, () => {
+  console.log("Listening");
 });
